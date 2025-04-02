7a681546fad9a1b968af388a1e343c669a23fd5c
pggh-01202326 <pgghqwerty@gmail.com>
2025-04-03 02:41:55 +0700

    [update ASP.net user]

--- DINHTHEVINH_2280603677/Program.cs
+++ DINHTHEVINH_2280603677/Program.cs
@@ -1,15 +1,22 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
 using DINHTHEVINH_2280603677.Models;
 using DINHTHEVINH_2280603677.Repositories;
 var builder = WebApplication.CreateBuilder(args);
 
-
+// Add services to the container.
+builder.Services.AddControllersWithViews();
 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-// Add services to the container.
-builder.Services.AddControllersWithViews();
+
+builder.Services.AddIdentity<IdentityUser, IdentityRole>()
+        .AddDefaultTokenProviders()
+        .AddDefaultUI()
+        .AddEntityFrameworkStores<ApplicationDbContext>();
+
+builder.Services.AddRazorPages();
 
 builder.Services.AddScoped<IProductRepository, EFProductRepository>();
 builder.Services.AddScoped<ICategoryRepository, EFCategoryRepository>();
@@ -27,6 +34,7 @@ app.UseRouting();
 
 app.UseAuthorization();
 
+app.MapRazorPages();
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Product}/{action=Index}/{id?}");
--- DINHTHEVINH_2280603677/obj/Debug/net8.0/DINHTHEVINH_2280603677.AssemblyInfo.cs
+++ DINHTHEVINH_2280603677/obj/Debug/net8.0/DINHTHEVINH_2280603677.AssemblyInfo.cs
@@ -11,10 +11,11 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.AspNetCore.Identity.UI.UIFrameworkAttribute("Bootstrap5")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("DINHTHEVINH_2280603677")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+30e4f3bef948acb3281933ee8afcf6e26ca1efc8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fedf37b64ba7257cdde728265c433697f260c29d")]
 [assembly: System.Reflection.AssemblyProductAttribute("DINHTHEVINH_2280603677")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DINHTHEVINH_2280603677")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
